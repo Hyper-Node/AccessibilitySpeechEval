d4048b7bd2bf9859958c79e1c21ea7c4ad5ca3b3
Stegmujo <johannesst@gmx.de>
2023-09-06 13:56:25 +0200

    All parsing html and hash fct

--- extract_html_to_json.js
+++ extract_html_to_json.js
@@ -2,7 +2,7 @@ const htmlp = require( 'node-html-parser' );
 const he = require('he'); // Import the 'he' library
 const fs = require('fs');
 const entities  = require('html-entities');
-
+const crypto = require('crypto');
 
 
 var fileContent;
@@ -12,7 +12,7 @@ try {
     console.error(err);
     process.exit(1);
 }
-
+var finalJSON = [];
 
 const root = htmlp.parse(fileContent);
 
@@ -27,21 +27,47 @@ for(var i=1;i<rows.length;i++){
     var mathML = row.querySelectorAll("td:nth-child(1) > math  ");
     var mathMLDefault = row.querySelectorAll("td:nth-child(2)  > pre ");
     var mathMLExplicit = row.querySelectorAll("td:nth-child(3)  > pre ");
-    var laTeX = row.querySelectorAll("td:nth-child(4) ");
+    var laTeX =decodeLaTeXColumn( row.querySelectorAll("td:nth-child(4) "));
+    var comment = decodeCommentColumn(row.querySelectorAll("td:nth-child(5)  > a  "));
+    var hashData = comment + laTeX;
+    var hash = crypto.createHash('md5').update(hashData).digest('hex');
 
     if(!mathML){
         continue;
     }
-    console.log( "MathML: \t" + mathML.toString());
-    console.log("MathMLDefault(0): \t" + decodePreString(mathMLDefault, 0));
-    console.log("MathMLExplicit(0): \t" + decodePreString(mathMLExplicit, 0));
-    console.log("LaTeX(0): \t" + laTeX.innerHTML.toString());
+    console.log("MathML: \t" + mathML.toString());
+    console.log("MathMLDefault(0): \t" + removeBold(decodePreString(mathMLDefault, 0)));
+    console.log("MathMLExplicit(0): \t" + removeBold(decodePreString(mathMLExplicit, 0)));
+    console.log("LaTeX(0): \t" + laTeX);
+    console.log("Name: \t" + comment);
+    console.log("UniqueID(md5): \t" + hash);
+}
 
-    //TBD: Remove bold annotations !!
+function removeBold(contentWithBold){
+    if(contentWithBold){
+        if(contentWithBold.indexOf("<b>bold</b>")!==-1){
+            // Skip bold replacement for the bold example where b tags are part of the formula
+            return contentWithBold;
+        }
+        return contentWithBold.replace(/<\/?b>/g, '');
+    }
+    return contentWithBold;
+}
 
-
-
-
+function decodeCommentColumn(comment){
+  if(comment[0] && comment[0].childNodes) {
+      return comment[0].childNodes[0].toString();
+  }else {
+      return comment.toString();
+  }
+}
+function decodeLaTeXColumn(laTeX) {
+    if (laTeX[0] && laTeX[0].childNodes) {
+        var latexString = laTeX[0].childNodes[0].toString();
+    } else {
+        var latexString = laTeX.toString();
+    }
+    return latexString;
 }
 function decodePreString(contOuter,index){
     if(!contOuter || !contOuter.length >=1){
@@ -58,4 +84,3 @@ function decodePreString(contOuter,index){
     return matches[0];
 }
 
-console.log(root.firstChild.structure);
